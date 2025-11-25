48ea6a50ed7fc9c5533c591cc63b941429e39227
Madhusudhankar <kmsreddy8639@gmail.com.com>
2025-11-25 22:20:13 +0530

    Home Page

---
+++ frontend_react/src/components/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+    <>
+       <div className='container'>
+        <div className='p-5 text-center bg-light-dark rounded'>
+        <div className='text-light '> Stock Prediction Portal</div>
+        <p className='text-light lead'> This Stock Prediction application utilizes Machine Learning Techniques and LSTMModel integrated with Django framework.It forecasts future Stock Prices by Analizing 100-day and 200-day moving Average.</p>
+         <Button text='Login' class='btn-outline-warning'/>
+           
+           </div>
+
+       </div>
+    </>
+  )
+}
+
+export default Main
